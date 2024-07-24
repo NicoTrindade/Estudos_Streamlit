275f7eed2bf69c90db5411d44d08ec52920700ec
Nielson Trindade <nicoctrindade@gmail.com>
2024-07-24 14:25:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import pandas as pd
+from funcoes import convert_pdf_to_txt_file, save_pages
 
 st.set_page_config(layout="wide")
 
 with st.sidebar:
     st.title("Conversão de PDF para CSV")
-    uploaded_file = st.file_uploader("Coloque o seu arquivo PDF aqui", type=['pdf'])
+    uploaded_file = st.file_uploader("Coloque o seu arquivo PDF aqui")
 
 if uploaded_file is not None:
-    path = pdf_file.read()
-    text_data_f, nbPages = convert_pdf_to_txt_file(pdf_file)
+    path = uploaded_file.read()
+    text_data_f, nbPages = convert_pdf_to_txt_file(uploaded_file)
+    
+    totalPages = "Pages: "+str(nbPages)+" in total"
+    st.info(totalPages)
+    st.download_button("Download txt file", text_data_f)
