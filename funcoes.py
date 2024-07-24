a2073c80adfe716256f02a641c68f3297dd04ef2
Nielson Trindade <nicoctrindade@gmail.com>
2024-07-24 14:24:41 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,33 @@
+import streamlit as st
+import pandas as pd
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+@st.cache_data
+def convert_pdf_to_txt_file(path):
+    texts = []
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    # fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    
+    file_pages = PDFPage.get_pages(path)
+    nbPages = len(list(file_pages))
+    for page in PDFPage.get_pages(path):
+      interpreter.process_page(page)
+      t = retstr.getvalue()
+    # text = retstr.getvalue()
+
+    # fp.close()
+    device.close()
+    retstr.close()
+    return t, nbPages
+
 def DadosRetornoCSV(TAMANHO_DADO, DADO_INICIO, DADO_FINAL, TEXTO_COMPLETO):
   DADOS_RESLTANTE = TEXTO_COMPLETO[DADO_INICIO + TAMANHO_DADO : DADO_FINAL-1]
   return DADOS_RESLTANTE.replace("\n", " ").strip()
