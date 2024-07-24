6e4b439e2f26a0ee9eac56ab6967f7dfac370e1b
Nielson Trindade <nicoctrindade@gmail.com>
2024-07-24 13:43:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ st.set_page_config(layout="wide")
 
 with st.sidebar:
     st.title("Conversão de PDF para CSV")
-    uploaded_file = st.file_uploader("Coloque o seu arquivo PDF aqui")
+    uploaded_file = st.file_uploader("Coloque o seu arquivo PDF aqui", type=['pdf'])
+
+if uploaded_file is not None:
+    path = pdf_file.read()
+    text_data_f, nbPages = convert_pdf_to_txt_file(pdf_file)
